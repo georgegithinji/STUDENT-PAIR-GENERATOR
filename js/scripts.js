58b9cd068c0b76970d611d4471f5fb0635938975
Brian Ngeno <ngenobrian7@gmail.com>
2018-03-07 09:21:49 +0300

    change function to fade change login info menu in and out when 'change password' button is pressed and fade out when 'change' button is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $(".change-login-info").click(function(event) {
     event.preventDefault();
-    $("#change-login-info").toggle();
+    $("#change-login-info").fadeToggle();
+  });
+  $(".reset").click(function(event){
+    $("#change-login-info").fadeToggle();
+    alert("Login info change successfull!")
   });
 });
